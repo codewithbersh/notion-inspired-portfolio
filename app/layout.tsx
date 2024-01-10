5a53eef4c24a332a8ef101c0e81f682dced318d6
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2024-01-10 13:26:10 +0300

    feat(font): add Geist font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '@/styles/globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'My personal portfolio',
-}
+  title: "Portfolio",
+  description: "My personal portfolio",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className="bg-background antialiased">{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ const HomePage = () => {
   return (
     <div>
       <Button>Test</Button>
+      <p className=" font-mono">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi quod
+        eius ullam atque dignissimos assumenda cum illum cumque doloremque
+        quasi. Magni facilis nam eaque recusandae perferendis magnam quae
+        possimus commodi?
+      </p>
     </div>
   );
 };
